7d154cde951d657d82f0e321064ee31154a9348c
Scott Geiger <scott.geiger@gmail.com>
2022-06-11 20:42:38 -0400

    Rewrote to use gpxpy functions for tracks/seg

---
+++ TODO.md
@@ -0,0 +1,13 @@
+# TODO
+
+## Add configuration file
+Add the abilty to specify defaults, such as directional settings, in a configuration file
+
+## Add ability to process folder/multiple GPX files
+Add the ability to process an entire folder or specify multiple GPX files.  Also include the parameter to create separate output files or join into a single output file.  Possibly use parameter: -1 for single file?
+
+## Add ability to "merge" tracks
+Tracks could be merged either within a single file or across multiple files
+
+## Add detection of secondary track crossing/junctions
+Use a boundary around the "main" track and determine if a point falls within the bounds.
--- gpxparse.py
+++ gpxparse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from collections import deque
+from distutils import extension
 from openpyxl import Workbook
 from openpyxl.styles import Font, DEFAULT_FONT
 from os.path import splitext
@@ -9,6 +10,7 @@ from re import sub
 import math, statistics
 import gpxpy
 import gpxpy.gpx
+import geopy
 from geopy.distance import geodesic
 from geopy.geocoders import Nominatim
 
@@ -23,11 +25,13 @@ parser = argparse.ArgumentParser(
           The output will be Excel, however you can specify CSV via the -c (--csv) optional command parameter.
     '''))
 parser.add_argument('gpxfile',help='Specify the gpx file to process')
+symbols = parser.add_mutually_exclusive_group()
 parser.add_argument('-c','--csv',action='store_true',help='Output as CSV instead of Excel')
+symbols.add_argument('-i','--include',action='append',help='Symbol to include.  Use multiple -i options for additional symbols.  Cannot be used with -x/--exclude')
 parser.add_argument('-r','--reverse',action='store_true',help='Reverse the direction of the track')
 parser.add_argument('-t','--track',help='Specify the track name to process')
 parser.add_argument('-v','--verbose',action='count',default=0,help='Verbose mode.  Multiple -v options increase the verbosity.  The maximum is 3')
-parser.add_argument('-x','--exclude',action='append',help='Symbol to exclude.  Use multiple -x options for additional symbols')
+symbols.add_argument('-x','--exclude',action='append',help='Symbol to exclude.  Use multiple -x options for additional symbols.  Cannot be used with -i/--include')
 args = vars(parser.parse_args())
 
 # 0: log level 3 (warning)
@@ -54,12 +58,12 @@ def camel_case(s):
   s = sub(r"[^0-9a-zA-Z]+", " ", s).title().replace(" ", "")
   return ''.join([s[0].lower(), s[1:]])
 
-def calc_bearing(pointA,pointB):
+def calc_bearing(pointA:gpxpy.gpx.GPXTrackPoint,pointB:gpxpy.gpx.GPXTrackPoint) -> float:
     deg2rad = math.pi/180
-    latA = pointA['latitude'] * deg2rad
-    latB = pointB['latitude'] * deg2rad
-    lonA = pointA['longitude'] * deg2rad
-    lonB = pointB['longitude'] * deg2rad
+    latA = pointA.latitude * deg2rad
+    latB = pointB.latitude * deg2rad
+    lonA = pointA.longitude * deg2rad
+    lonB = pointB.longitude * deg2rad
     delta_ratio = math.log(math.tan(latB/ 2 + math.pi / 4) / math.tan(latA/ 2 + math.pi / 4))
     delta_lon = abs(lonA - lonB)
 
@@ -77,65 +81,65 @@ except:
 gpx = gpxpy.parse(gpx_file)
 
 # Get track to process
-trk = None
+trkNum = None
 if args.get('track') is not None:
-    for track in gpx.tracks:
-        if track.name == args.get('track'): trk = track
-    if trk is None:
+    for i,track in enumerate(gpx.tracks):
+        if track.name == args.get('track'): trkNum = i
+    if trkNum is None:
         print('\nNo track with that name found, please make a selection.')
 
-while trk is None:
+while trkNum is None:
     print('\nTrack List for {}:'.format(args.get('gpxfile')))
     for i,track in enumerate(gpx.tracks):
         print('  {0}.\t{1}'.format(int(i)+1,track.name))
     print('\n  X.\tExit')
     n = input('\nEnter the # of the track to process: ')
     if n == 'x' or n == 'X': sys.exit(0)
-    try:
-        trk = gpx.tracks[int(n)-1]
-        print('')
-    except:
+    if int(n) > len(gpx.tracks):
         print('Invalid track selection.  Please select a valid track #')
-        trk = None
+        trkNum = None
+    else:
+        trkNum = int(n)-1
+        print('')
 
-logger.info('Beginning Processing of {0} for track {1}'.format(args.get('gpxfile'),trk.name))
+track = gpx.tracks[trkNum]
+logger.info('Beginning Processing of {0} for track {1}'.format(args.get('gpxfile'),track.name))
 
 # Collect Waypoints
 logger.info('Processing waypoints...')
-#waypoints = list()
 waypoints = deque()
 wpt_symbols = dict()
 for waypoint in gpx.waypoints:
-    if waypoint.symbol not in args.get('exclude'):
+    # included/exclude waypoint symbols
+    if (args.get('include') is not None and waypoint.symbol in (args.get('include') or [])) or (args.get('exclude') is not None and waypoint.symbol not in (args.get('exclude') or [])):
         waypoints.append({'name':waypoint.name,'description':waypoint.description,'symbol':waypoint.symbol,'latitude':waypoint.latitude,'longitude':waypoint.longitude,'nearest_trkpoint':None,'nearest_distance':math.inf})
         wpt_symbols[camel_case(waypoint.symbol)] = waypoint.symbol
 #    geolocator = Nominatim(user_agent="Finger Lakes Trail Mapper")
 #    location = geolocator.reverse('{0},{1}'.format(waypoint.latitude, waypoint.longitude))
 #    print(location.raw)
 logger.info('Including {0} Symbols: "{1}"'.format(len(wpt_symbols),'","'.join(sorted(wpt_symbols.values()))))
-logger.info('Ignoring {0} Symbols: "{1}"'.format(len(args.get('exclude')),'","'.join(sorted(args.get('exclude')))))
+if args.get('exclude') is not None: logger.info('Ignoring {0} Symbols: "{1}"'.format(len(args.get('exclude')),'","'.join(sorted(args.get('exclude')))))
 logger.info('Processed {} waypoints'.format(len(waypoints)))
 
 # Collect Track Point
-logger.info('Processing trakpoints for {}...'.format(trk.name))
-#trkpoints = list()
+logger.info('Processing trakpoints for {}...'.format(track.name))
 trkpoints = deque()
-for track in gpx.tracks:
-    if track.name == trk.name:
-        for segment in track.segments:
-            for point in segment.points:
-                trkpoints.append({'latitude':point.latitude,'longitude':point.longitude,'near_waypoint':None,'distance_from_waypoint':None})
-logger.info('Processed {} trackpoints'.format(len(trkpoints)))
+pointNum = 0;
+for point,s,n in track.walk():
+    trkpoints.append({'number':n,'point':point,'near_waypoint':None,'distance_from_waypoint':None})
+    pointNum = n
+totalPoints = int(pointNum)+1
+logger.info('Processed {} trackpoints'.format(totalPoints))
 
 # reverse the track direction if option indicated
 if args.get('reverse'):
     logger.warning('Reversing track direction')
     trkpoints.reverse()
 
-# Determine closest trackpoint to waypoint
+# Find closest waypoint to trackpoint and update both collections
 for i,waypoint in enumerate(waypoints):
     for j,trkpoint in enumerate(trkpoints):
-        dist = geodesic((trkpoint['latitude'],trkpoint['longitude']),(waypoint['latitude'],waypoint['longitude'])).feet
+        dist = geodesic((trkpoint['point'].latitude,trkpoint['point'].longitude),(waypoint['latitude'],waypoint['longitude'])).feet
         if dist < waypoint['nearest_distance']:
             waypoints[i]['nearest_trkpoint'] = j
             waypoints[i]['nearest_distance'] = dist
@@ -144,55 +148,56 @@ for k,waypoint in enumerate(waypoints):
         trkpoints[waypoint['nearest_trkpoint']]['near_waypoint'] = k
         trkpoints[waypoint['nearest_trkpoint']]['distance_from_waypoint'] = waypoint['nearest_distance']
 
-
-#Build segments
-logger.info('Building segments...')
-logger.info('-'*60)
-segments = deque()
-track_distance = 0
-segment_distance = 0
-measure = deque()
+# Build segments
+gpx_track = gpxpy.gpx.GPXTrack()
+seg = deque()
+#seg.append({'seg':gpxpy.gpx.GPXTrackSegment(),'wpt':{'name':'Start','description':'start'}})
+#gpx_track.segments.append(seg[0]['seg'])
+seg.append({'seg':gpxpy.gpx.GPXTrackSegment(),'wpt':None})
+gpx_track.segments.append(seg[0]['seg'])
+s = 0
 for i,trkpoint in enumerate(trkpoints):
-    measure.append(trkpoint)
-    if len(measure) == 2:
-        dist = geodesic((measure[0]['latitude'],measure[0]['longitude']),(measure[1]['latitude'],measure[1]['longitude'])).miles
-        track_distance += dist
-        segment_distance += dist
-        #del measure[0]
-        measure.popleft()
-    wpt = trkpoint['near_waypoint']
-    if wpt is not None:
-        try:
-            h0 = calc_bearing(trkpoints[i-3],trkpoints[i])
-            h1 = calc_bearing(trkpoints[i],trkpoints[i+3])        
-            dH = ((h1-h0)+360)%360
-            #TODO: need to reverse directions with reverse
-            direction = ''
-            dirlist = ['continue straight','slight right','turn right','sharp right','u-turn to the right','turn around','u-turn to the left','sharp left','turn left','slight left','continue straight']
-            if args.get('reverse'): dirlist.reverse()
-            if dH >= 0 and dH <= 10: direction = dirlist[0]
-            if dH >= 11 and dH <= 30: direction = dirlist[1] #slight (20)
-            if dH >= 31 and dH <= 110: direction = dirlist[2] #turn (80)
-            if dH >= 111 and dH <= 160: direction = dirlist[3] #sharp (50)
-            if dH >= 161 and dH <= 170: direction = dirlist[4] #uturn (10)
-            if dH >= 171 and dH <= 190: direction = dirlist[5]
-            if dH >= 191 and dH <= 200: direction = dirlist[6] #uturn
-            if dH >= 201 and dH <= 250: direction = dirlist[7] #sharp
-            if dH >= 251 and dH <= 330: direction = dirlist[8] #turn
-            if dH >= 331 and dH <= 350: direction = dirlist[9] #slight
-            if dH >= 351 and dH <= 360: direction = dirlist[10]
-            segments.append([waypoints[wpt]['name'],waypoints[wpt]['description'],direction,round(segment_distance,1),round(track_distance,1)])
-            segment_distance = 0
-        except:
-            # if i < 3 then we don't need to measure the segment
-            pass
+    if trkpoint['near_waypoint'] is not None:
+        seg[s]['wpt'] = waypoints[trkpoint['near_waypoint']]
+        s += 1
+        seg.append({'seg':gpxpy.gpx.GPXTrackSegment(),'wpt':None})
+        gpx_track.segments.append(seg[s]['seg'])
+    seg[s]['seg'].points.append(trkpoint['point'])
+seg.pop()
+gpx_track.segments.pop()
 
-for segment in segments:
-    logger.info(', '.join(str(e) for e in segment))
-
-logger.info('-'*60)
-logger.info('Total Track Distance (in miles): {}'.format(round(track_distance,2)))
-
+runTotal = 0
+rows = []
+for i,segment in enumerate(gpx_track.segments):
+    direction = ''
+    segDist = geopy.units.mi(meters=segment.length_2d())
+    if args.get('csv'):
+        runTotal += segDist
+    else: 
+        runTotal = '=INDIRECT(ADDRESS(ROW(),COLUMN()-1))' if i == 0 else '=INDIRECT(ADDRESS(ROW(),COLUMN()-1))+INDIRECT(ADDRESS(ROW()-1,COLUMN()))'
+    if i < len(gpx_track.segments)-1:
+        cs_lastp = segment.get_points_no()
+        segment2 = seg[i+1]['seg']
+        h0 = None if cs_lastp < 2 else calc_bearing(segment.points[cs_lastp-3],segment.points[cs_lastp-1])
+        h1 = None if segment2.get_points_no() < 2 else calc_bearing(segment2.points[0],segment2.points[2])
+        dH = h0 or h1
+        if h0 is not None and h1 is not None: dH = ((h1-h0)+360)%360
+        dirlist = ['continue straight','slight right','turn right','sharp right','u-turn to the right','turn around','u-turn to the left','sharp left','turn left','slight left','continue straight']
+        if args.get('reverse'): dirlist.reverse()
+        if dH >= 0 and dH <= 10: direction = dirlist[0]
+        if dH > 10 and dH <= 30: direction = dirlist[1] #slight 
+        if dH > 30 and dH <= 110: direction = dirlist[2] #turn
+        if dH > 110 and dH <= 160: direction = dirlist[3] #sharp 
+        if dH > 160 and dH <= 170: direction = dirlist[4] #uturn
+        if dH > 170 and dH <= 190: direction = dirlist[5]
+        if dH > 190 and dH <= 200: direction = dirlist[6] #uturn
+        if dH > 200 and dH <= 250: direction = dirlist[7] #sharp
+        if dH > 250 and dH <= 330: direction = dirlist[8] #turn
+        if dH > 330 and dH <= 350: direction = dirlist[9] #slight
+        if dH > 350 and dH <= 360: direction = dirlist[10]
+    rows.append([seg[i]['wpt']['name'],seg[i]['wpt']['description'],direction,segDist,runTotal])
+    
+# Generate Output File Name
 ext = 'csv' if args.get('csv') else 'xlsx'
 if args.get('reverse'):
     filename = splitext(args.get('gpxfile'))[0] + '_reverse.'+ext
@@ -200,13 +205,14 @@ else:
     filename = splitext(args.get('gpxfile'))[0] + '.'+ext
 
 fldnames = ["Way Point Name","Description","Direction","Segment","Running Total"]
-if args.get('csv'):
+runTotal = 0
+if args.get('csv'): # CSV Output
     logger.info('Writing data to CSV file: {}...'.format(filename))
     csvfile = open(filename,'w')
     writer = csv.writer(csvfile,delimiter=',',quoting=csv.QUOTE_ALL)
     writer.writerow(fldnames)
-    for segment in segments:
-        writer.writerow(segment)
+    for rowData in rows:
+        writer.writerow(rowData)
     csvfile.close()
     logger.info('Finished writing to CSV file')
 else: # Excel Output
@@ -218,13 +224,8 @@ else: # Excel Output
     ws.append(fldnames)
     ws.row_dimensions[1].font = Font(name='Arial',bold=True)
     row = 2
-    for i,segment in enumerate(segments):
-        segment.pop()
-        if i == 0:
-            segment.append('=INDIRECT(ADDRESS(ROW(),COLUMN()-1))')
-        else:
-            segment.append('=INDIRECT(ADDRESS(ROW(),COLUMN()-1))+INDIRECT(ADDRESS(ROW()-1,COLUMN()))')
-        ws.append(segment)
+    for rowData in rows:
+        ws.append(rowData)
         row += 1
     ws.column_dimensions['A'].width = 20
     ws.column_dimensions['B'].width = 40
